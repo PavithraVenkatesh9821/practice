0745dfb598f511b2683c8e452807027cac5986ac
PavithraVenkatesh9821 <Pavithraswtest98@gmail.com>
2024-03-12 07:45:46 +0530

    msg

--- src/test/java/org/testt/traini.java
+++ src/test/java/org/testt/traini.java
@@ -4,5 +4,7 @@ public class traini {
 public static void main(String[] args) {
 	System.out.println("123");
 	System.out.println("456");
+	System.out.println("67890");
+	System.out.println("yhjgfdfd");
 }
 }
